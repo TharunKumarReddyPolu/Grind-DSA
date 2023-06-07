ac54910858c782302d69bd9a35b493166e07c62d
Polu Tharun Kumar Reddy <polutharun528@gmail.com>
2023-06-06 21:32:24 -0400

    added code for top_k_freq_elements

---
+++ Blind_75/Arrays & Hashing/top_k_frequent_elements.py
@@ -0,0 +1,32 @@
+'''
+Problem Statement: https://leetcode.com/problems/top-k-frequent-elements/
+
+Input: nums = [1,1,1,2,2,3], k = 2
+Output: [1,2]
+'''
+
+from typing import List
+
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        count ={}
+        freq = [[] for i in range(len(nums)+1)]
+        for i in nums:
+            # can use count[i] = 1 + count.get(i,0) or below
+            if i in count:
+                count[i] += 1
+            else:
+                count[i] = 1
+        for i, c in count.items():
+            freq[c].append(i)
+
+        res = []
+        for i in range(len(freq)-1, 0, -1):
+            for n in freq[i]:
+                res.append(n)
+                if len(res) == k:
+                    return res
+    
+s = Solution()
+print(s.topKFrequent([1,1,1,2,2,3], 2))

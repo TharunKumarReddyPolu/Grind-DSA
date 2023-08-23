64d2b8d4c70610df41a27de5c974c35424d99391
Polu Tharun Kumar Reddy <polutharun528@gmail.com>
2023-08-23 18:38:17 -0400

    done with lca bst

---
+++ Blind_75/Trees/lca_bst.py
@@ -0,0 +1,30 @@
+'''
+Problem Statement: https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
+
+Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 4
+Output: 2
+Explanation: The LCA of nodes 2 and 4 is 2, since a node can be a descendant of itself according to the LCA definition.
+'''
+
+from typing import List, Optional
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        curr = root
+
+        while curr:
+            if p.val > curr.val and q.val > curr.val:
+                curr = curr.right
+            elif p.val < curr.val and q.val < curr.val:
+                curr = curr.right
+            else:
+                return curr
+
+    

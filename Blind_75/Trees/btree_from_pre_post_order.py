a6802fbef7b74f045edfe122da5dd4d96d3fee43
Polu Tharun Kumar Reddy <polutharun528@gmail.com>
2023-08-25 19:46:21 -0400

    done with btree from pre and post order

---
+++ Blind_75/Trees/btree_from_pre_post_order.py
@@ -0,0 +1,34 @@
+'''
+Problem Statement: https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/
+
+Input: preorder = [3,9,20,15,7], inorder = [9,3,15,20,7]
+Output: [3,9,20,null,null,15,7]
+'''
+
+import collections
+from typing import List, Optional
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        if not preorder or not inorder:
+            return None
+        
+        root = TreeNode(preorder[0])
+        mid = inorder.index(preorder[0])
+
+        root.left = self.buildTree(preorder[1:mid+1], inorder[:mid])
+        root.right = self.buildTree(preorder[mid+1:], inorder[mid+1:])
+
+        return root
+
+
+
+
+    

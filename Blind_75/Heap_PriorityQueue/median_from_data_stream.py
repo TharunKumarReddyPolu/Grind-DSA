55048b18ebd6c30e9ee849c7e3264427e7169998
Polu Tharun Kumar Reddy <polutharun528@gmail.com>
2023-08-30 16:14:48 -0400

    done with median from data stream

---
+++ Blind_75/Heap_PriorityQueue/median_from_data_stream.py
@@ -0,0 +1,43 @@
+'''
+Problem Statement: https://leetcode.com/problems/find-median-from-data-stream/
+
+Input
+["MedianFinder", "addNum", "addNum", "findMedian", "addNum", "findMedian"]
+[[], [1], [2], [], [3], []]
+Output
+[null, null, null, 1.5, null, 2.0]
+
+Explanation
+MedianFinder medianFinder = new MedianFinder();
+medianFinder.addNum(1);    // arr = [1]
+medianFinder.addNum(2);    // arr = [1, 2]
+medianFinder.findMedian(); // return 1.5 (i.e., (1 + 2) / 2)
+medianFinder.addNum(3);    // arr[1, 2, 3]
+medianFinder.findMedian(); // return 2.0
+
+'''
+
+import collections
+from typing import List, Optional
+
+class MedianFinder:
+
+    def __init__(self):
+        pass
+
+    def addNum(self, num: int) -> None:
+        pass
+
+    def findMedian(self) -> float:
+        pass
+
+
+# Your MedianFinder object will be instantiated and called as such:
+# obj = MedianFinder()
+# obj.addNum(num)
+# param_2 = obj.findMedian()
+
+
+
+
+    

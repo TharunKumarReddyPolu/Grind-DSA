d1482b29bfc7d2b438c39146cd719209d03881c8
Polu Tharun Kumar Reddy <polutharun528@gmail.com>
2023-11-07 16:03:38 -0500

    done with median from data stream

--- Blind_75/Heap_PriorityQueue/median_from_data_stream.py
+++ Blind_75/Heap_PriorityQueue/median_from_data_stream.py
@@ -19,18 +19,38 @@ medianFinder.findMedian(); // return 2.0
 
 import collections
 from typing import List, Optional
+import heapq
 
 class MedianFinder:
 
     def __init__(self):
-        pass
+        # two heaps, large, small which are minheap and maxheap
+        # heaps should be equal size
+        self.small, self.large = [], []
 
     def addNum(self, num: int) -> None:
-        pass
+        heapq.heappush(self.small, -1 * num) # multiply with -1 to make a maxheap, python implements minheap by default
+
+        # maintain every num small <= every num large
+        if (self.small and self.large and (-1 * self.small[0]) > self.large[0]):
+            val = -1 * heapq.heappop(self.small)
+            heapq.heappush(self.large, val)
+        
+        # uneven size?
+        if len(self.small) > len(self.large) + 1:
+            val = -1 * heapq.heappop(self.small)
+            heapq.heappush(self.large, val)
+        if len(self.large) > len(self.small) + 1:
+            val = heapq.heappop(self.large)
+            heapq.heappush(self.small, -1 * val)
 
     def findMedian(self) -> float:
-        pass
+        if len(self.small) > len(self.large):
+            return self.small[0]
+        if len(self.large) > len(self.small):
+            return self.large[0]
 
+        return (-1 * self.small[0] + self.large[0]) / 2
 
 # Your MedianFinder object will be instantiated and called as such:
 # obj = MedianFinder()

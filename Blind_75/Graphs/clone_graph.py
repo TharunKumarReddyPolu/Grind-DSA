ad88ba33fbddee7592bbd8f3456ce08bbc6fe7b4
Polu Tharun Kumar Reddy <polutharun528@gmail.com>
2023-11-11 16:08:52 -0500

    done with clone graph

---
+++ Blind_75/Graphs/clone_graph.py
@@ -0,0 +1,40 @@
+'''
+Problem Statement: https://leetcode.com/problems/clone-graph/
+
+Input: adjList = [[2,4],[1,3],[2,4],[1,3]]
+Output: [[2,4],[1,3],[2,4],[1,3]]
+Explanation: There are 4 nodes in the graph.
+1st node (val = 1)'s neighbors are 2nd node (val = 2) and 4th node (val = 4).
+2nd node (val = 2)'s neighbors are 1st node (val = 1) and 3rd node (val = 3).
+3rd node (val = 3)'s neighbors are 2nd node (val = 2) and 4th node (val = 4).
+4th node (val = 4)'s neighbors are 1st node (val = 1) and 3rd node (val = 3).
+
+'''
+
+from typing import List, Optional
+
+
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+
+
+from typing import Optional
+
+class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        oldToNew = {}
+
+        def dfs(node):
+            if node in oldToNew:
+                return oldToNew[node]
+            
+            copy = Node(node.val)
+            oldToNew[node] = copy
+            for neigh in node.neighbors:
+                copy.neighbors.append(dfs(neigh))
+            return copy
+        
+        return dfs(node) if node else None

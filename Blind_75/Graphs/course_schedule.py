5c23f358e710dbc0bb1ecff6fa541efe041dbfbf
Polu Tharun Kumar Reddy <polutharun528@gmail.com>
2023-11-13 21:11:59 -0500

    done with course schedule

---
+++ Blind_75/Graphs/course_schedule.py
@@ -0,0 +1,36 @@
+'''
+Problem Statement: https://leetcode.com/problems/course-schedule/
+
+Input: numCourses = 2, prerequisites = [[1,0]]
+Output: true
+Explanation: There are a total of 2 courses to take. 
+To take course 1 you should have finished course 0. So it is possible.
+'''
+
+from typing import List
+
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        preMap = {i: [] for i in range(numCourses)}
+        for crs, pre in prerequisites:
+            preMap[crs].append(pre)
+        
+        visited = set()
+
+        def dfs(crs):
+            if crs in visited:
+                return False
+            if preMap[crs] == []:
+                return True
+            
+            visited.add(crs)
+            for pre in preMap[crs]:
+                if not dfs(pre): return False
+            visited.remove(crs)
+            preMap[crs] = []
+            return True
+        
+        for crs in range(numCourses):
+            if not dfs(crs):
+                return False
+        return True

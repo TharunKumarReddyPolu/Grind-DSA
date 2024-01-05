c86ee1ea8fc0ee314bfa41353b38c8d9ae190da7
Polu Tharun Kumar Reddy <polutharun528@gmail.com>
2024-01-05 04:25:07 -0500

    fixed the error

--- Blind_75/1D DP/max_prod_subarray.py
+++ Blind_75/1D DP/max_prod_subarray.py
@@ -10,13 +10,13 @@ from typing import List
 
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
-        res = max(nums)
+        res = nums[0]
         currMin, currMax = 1, 1
 
         for n in nums:
             tmp = currMax * n
             currMax = max(n * currMax, n * currMin, n)
-            currMin = min(n * tmp, n * currMin, n)
+            currMin = min(tmp, n * currMin, n)
             res = max(res, currMax)
         
         return res

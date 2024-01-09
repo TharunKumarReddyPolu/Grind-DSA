7af65506caf9fba9c403a55afdcfb313d05b7e34
Polu Tharun Kumar Reddy <polutharun528@gmail.com>
2024-01-09 05:46:57 -0500

    done with unique paths

---
+++ Blind_75/2D DP/unique_paths.py
@@ -0,0 +1,27 @@
+'''
+Problem Statement: https://leetcode.com/problems/unique-paths/
+
+Input: m = 3, n = 2
+Output: 3
+Explanation: From the top-left corner, there are a total of 3 ways to reach the bottom-right corner:
+1. Right -> Down -> Down
+2. Down -> Down -> Right
+3. Down -> Right -> Down
+'''
+
+from typing import List
+
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        row = [1] * n
+
+        for i in range(m - 1):
+            newRow = [1] * n
+            for j in range(n-2, -1, -1):
+                newRow[j] = newRow[j + 1] + row[j]
+            row = newRow
+    
+        return row[0]
+    
+sol = Solution()
+print(sol.uniquePaths(3, 2))

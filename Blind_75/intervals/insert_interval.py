ef344e822cef2eb37dc2d161951a5b5fd7fa2036
Polu Tharun Kumar Reddy <polutharun528@gmail.com>
2024-01-10 00:13:29 -0500

    fixed bug in insert interval

--- Blind_75/intervals/insert_interval.py
+++ Blind_75/intervals/insert_interval.py
@@ -18,7 +18,7 @@ class Solution:
                 res.append(newInterval)
                 return res + intervals[i:]
             elif newInterval[0] > intervals[i][1]:
-                res.append(newInterval)
+                res.append(intervals[i])
             else:
                 newInterval = [min(newInterval[0], intervals[i][0]), max(newInterval[1], intervals[i][1])]
         

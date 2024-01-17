54b278fdcb3474fbc1eeac7c0790cede4ed2b1ea
Polu Tharun Kumar Reddy <polutharun528@gmail.com>
2024-01-16 23:43:12 -0500

    done with counting bits

---
+++ Blind_75/Bit Manipulation/counting_bits.py
@@ -0,0 +1,31 @@
+'''
+Problem Statement: https://leetcode.com/problems/counting-bits/description/
+
+Input: n = 5
+Output: [0,1,1,2,1,2]
+Explanation:
+0 --> 0
+1 --> 1
+2 --> 10
+3 --> 11
+4 --> 100
+5 --> 101
+'''
+
+from typing import List
+
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        dp = [0] * (n + 1)
+        offset = 1
+
+        for i in range(1, n + 1):
+            if offset * 2 == i:
+                offset = i
+            dp[i] = 1 + dp[i - offset]
+
+        return dp       
+
+
+sol = Solution()
+print(sol.countBits(5))
